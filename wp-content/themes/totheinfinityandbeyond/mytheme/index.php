18c7f8067dbdd864086dbf44fd21bb7147064fb4
MARCO DELPOPOLO <156794727+MarcoDelpimme@users.noreply.github.com>
2024-05-06 10:54:56 +0200

    adding. commit 2

--- wp-content/themes/totheinfinityandbeyond/mytheme/index.php
+++ wp-content/themes/totheinfinityandbeyond/mytheme/index.php
@@ -176,63 +176,7 @@ get_header(); ?>
 <button><a href="<?php echo esc_url(get_permalink(get_page_by_path('aggiungi-articolo'))); ?>" class="button">Aggiungi Articolo</a>
 </button>
 
-<?php
-/*
-Template Name: Nuovo Articolo
-*/
-get_header();
-?>
 
-<div id="primary" class="content-area">
-    <main id="main" class="site-main" role="main">
-
-        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-            <header class="entry-header">
-                <h1 class="entry-title"><?php the_title(); ?></h1>
-            </header><!-- .entry-header -->
-
-            <div class="entry-content">
-                <form method="post" action="">
-                    <label for="titolo">Titolo:</label><br>
-                    <input type="text" id="titolo" name="titolo" required><br><br>
-
-                    <label for="contenuto">Contenuto:</label><br>
-                    <textarea id="contenuto" name="contenuto" rows="8" cols="50" required></textarea><br><br>
-
-                    <input type="submit" name="submit" value="Pubblica Articolo">
-                </form>
-
-                <?php
-                if (isset($_POST['submit'])) {
-                    $titolo = sanitize_text_field($_POST['titolo']);
-                    $contenuto = wp_kses_post($_POST['contenuto']);
-
-                    $nuovo_articolo = array(
-                        'post_title' => $titolo,
-                        'post_content' => $contenuto,
-                        'post_status' => 'publish',
-                        'post_author' => 1
-                    );
-
-                    $post_id = wp_insert_post($nuovo_articolo);
-
-                    if ($post_id) {
-                        echo "Articolo pubblicato con successo!";
-                    } else {
-                        echo "Si è verificato un errore durante la pubblicazione dell'articolo.";
-                    }
-                }
-                ?>
-            </div><!-- .entry-content -->
-
-        </article><!-- #post-<?php the_ID(); ?> -->
-
-    </main><!-- #main -->
-</div><!-- #primary -->
-
-<?php
-get_footer();
-?>
 
 
 
--- wp-content/themes/totheinfinityandbeyond/mytheme/single.php
+++ wp-content/themes/totheinfinityandbeyond/mytheme/single.php
@@ -16,4 +16,56 @@ the_post(); ?>
 </div>
 
 <?php
+/*
+Template Name: Aggiungi Articolo
+*/
+
+if (isset($_POST['submit'])) {
+    $titolo = sanitize_text_field($_POST['titolo']);
+    $contenuto = wp_kses_post($_POST['contenuto']);
+
+    $nuovo_articolo = array(
+        'post_title' => $titolo,
+        'post_content' => $contenuto,
+        'post_status' => 'pending',
+        'post_author' => get_current_user_id()
+    );
+
+    $post_id = wp_insert_post($nuovo_articolo);
+
+    if ($post_id) {
+        echo "Articolo pubblicato con successo!";
+    } else {
+        echo "Si è verificato un errore durante la pubblicazione dell'articolo.";
+    }
+}
+?>
+
+<div id="primary" class="content-area">
+    <main id="main" class="site-main" role="main">
+
+        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+            <header class="entry-header">
+                
+            </header>
+
+            <div class="entry-content">
+                <form method="post" action="">
+                    <label for="titolo">Titolo:</label><br>
+                    <input type="text" id="titolo" name="titolo" required><br><br>
+
+                    <label for="contenuto">Contenuto:</label><br>
+                    <textarea id="contenuto" name="contenuto" rows="8" cols="50" required></textarea><br><br>
+
+                    <input type="submit" name="submit" value="Pubblica Articolo">
+                </form>
+            </div>
+
+        </article><!-- #post-<?php the_ID(); ?> -->
+
+    </main>
+</div>
+
+
+<?php
 get_footer();

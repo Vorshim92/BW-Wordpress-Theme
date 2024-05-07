b549c29ec0dbc45f66d530708a442bc244f43c74
Vorshim92 <vorshim92@gmail.com>
2024-05-07 17:44:07 +0200

    marco del popolo si lamenta

--- wp-content/themes/totheinfinityandbeyond/single-ttiab_trips.php
+++ wp-content/themes/totheinfinityandbeyond/single-ttiab_trips.php
@@ -143,46 +143,12 @@ $args = array(
                 ?>
             </div>
         </div>
-        <div class="col-12 col-md-4 border p-4 rounded">
-            <?php
-            /*
-            Template Name: Aggiungi Articolo
-            */
 
-            if (isset($_POST['submit'])) {
-                $titolo = sanitize_text_field($_POST['titolo']);
-                $contenuto = wp_kses_post($_POST['contenuto']);
-                $nuovo_articolo = array(
-                    'post_title' => $titolo,
-                    'post_content' => $contenuto,
-                    'post_status' => 'pending',
-                    'post_author' => get_current_user_id(),
-                    'post_type' => 'ttiab_reviews',
-                    'post_category' => $current_post_categories,
-                    'post-thumbnails' => "url"
-                );
-                $post_id = wp_insert_post($nuovo_articolo);
-                if ($post_id) {
-                    echo "Articolo pubblicato con successo!";
-                } else {
-                    echo "Si è verificato un errore durante la pubblicazione dell'articolo.";
-                }
-            }
-            ?>
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <p class="text-secondary">Hai vissuto questa esperienza con noi e vuoi pubblicare il tuo articolo?</p>
-                <p class="text-secondary">qui puoi farlo <a href="#" class="fw-light">Leggi la politica delle recensioni</a> </p>
-                <form method="post" action="">
-                    <label for="titolo">Titolo:</label><br>
-                    <input type="text" id="titolo" name="titolo" required><br><br>
-
-                    <label for="contenuto">Contenuto:</label><br>
-                    <textarea id="contenuto" name="contenuto" style="width:100%;height:10rem" required></textarea><br><br>
-
-                    <input type="submit" name="submit" value="Pubblica Articolo" style="background-color:#E09F05" class="border-0 text-white rounded p-1">
-                </form>
-            </article><!-- #post-<?php the_ID(); ?> -->
-        </div>
+    </div>
+    <div class="col-12 col-md-4">
+        <?php
+        echo do_shortcode('[custom_add_review]');
+        ?>
     </div>
 </div>
 

56126ea0e961f230166c16829ed8f4ac6906eaab
Vorshim92 <vorshim92@gmail.com>
2024-05-07 16:21:05 +0200

    fix

--- wp-content/themes/totheinfinityandbeyond/single.php
+++ wp-content/themes/totheinfinityandbeyond/single.php
@@ -8,7 +8,7 @@ the_post(); ?>
     <a href="<?php the_author_meta() ?>"></a>
     <img style="width:100% ;max-height:25rem;object-fit:cover" src="<?php the_post_thumbnail_url('custom-cover-thumbnail'); ?>" alt="<?php the_title(); ?>">
     <div class="row pt-4 border-bottom">
-        <div class="col-12 col-md-9"> 
+        <div class="col-12 col-md-9">
 
             <?php the_content(); ?>
         </div>
@@ -23,42 +23,41 @@ the_post(); ?>
             <div class="row gy-2">
 
 
-                <?php
-             {
-                $args = array(
-                            'posts_per_page' => 5,
-                            'category__in' => $current_post_categories,
-                            'post__not_in' => array($current_post_id)
-                        );
+                <?php {
+                    $args = array(
+                        'posts_per_page' => 5,
+                        'category__in' => $current_post_categories,
+                        'post__not_in' => array($current_post_id)
+                    );
 
-                $sidebar_posts = new WP_Query($args);
-                while ($sidebar_posts->have_posts()):
-                    $sidebar_posts->the_post();
+                    $sidebar_posts = new WP_Query($args);
+                    while ($sidebar_posts->have_posts()) :
+                        $sidebar_posts->the_post();
 
-                  
 
-                        ?>
+
+                ?>
                         <div class="col-12 border-bottom pb-2">
                             <div class="row">
                                 <div class="col-6">
-                                    <div class="multiply rounded"
-                                        style="width: 100%; height: 8rem; background-image: url('<?php the_post_thumbnail_url('thumbnail', ['id' => 'img-state']); ?>');">
+                                    <div class="multiply rounded" style="width: 100%; height: 8rem; background-image: url('<?php the_post_thumbnail_url('thumbnail', ['id' => 'img-state']); ?>');">
                                     </div>
                                 </div>
                                 <div class="col-6">
-                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                <p class="text-secondary">Autore: <?php the_author(); ?></p>
-                                <p class="text-secondary">Pubblicato il: <?php the_date(); ?></p>
+                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                    <p class="text-secondary">Autore: <?php the_author(); ?></p>
+                                    <p class="text-secondary">Pubblicato il: <?php the_date(); ?></p>
                                 </div>
                             </div>
 
                         </div>
 
-                        <?php
-                    
-                endwhile;
-                wp_reset_postdata(); }
-               ?>
+                <?php
+
+                    endwhile;
+                    wp_reset_postdata();
+                }
+                ?>
 
             </div>
         </div>
@@ -72,16 +71,13 @@ the_post(); ?>
 
 ?>
 
-<div class="container pt-5">
-    <h5 class="text-secondary">Se hai trovato utile questo articolo</h5>
 
 
-    <?php if (comments_open() || get_comments_number()):
-        comments_template();
-    endif ?>
+<?php if (comments_open() || get_comments_number()) :
+    comments_template();
+endif ?>
 
 
-</div>
 
 
 

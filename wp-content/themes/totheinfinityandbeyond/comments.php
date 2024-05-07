f2d874c3146a92a122482b2164b7fc149682a574
ALFIO BATTIATO <battiato3@gmail.com>
2024-05-07 16:15:08 +0200

    qq

--- wp-content/themes/totheinfinityandbeyond/comments.php
+++ wp-content/themes/totheinfinityandbeyond/comments.php
@@ -4,39 +4,15 @@
         <p class="">Se hai trovato utile questo articolo</p>
 
         <div class="row commenti">
-            <div class="col-md-8">
+            <div class="col-6">
                 <div class="comment-form">
                     <?php comment_form(); ?>
                 </div>
             </div>
 
-            <div class="col-md-4 p-2 text-center ">
-                <h1>WeTravel i numeri</h1>
-                <div class="d-flex pt-5 gap-5 we">
-                    <div>
-                        <h2 class="display-6">250+</h2>
-                        <p>Itinerari in tutto il mondo</p>
 
-                    </div>
-                    <div class="border-start border-end px-2">
-                        <h2 class="display-6 ">100.000</h2>
-                        <p>Persone in tutta Europa sono già partite con noi </p>
-                     
-
-                    </div>
-                    <div>
-                        <h2 class="display-6">4.7</h2>
-                        <p>Gradimento dei nostri viaggi</p>
-
-                    </div>
-
-
-
-                </div>
-
-            </div>
-            <hr>
-            <div class="col-12 mt-5 ">
+            
+            <div class="col-6 ">
                 <p>Commenti dei nostri utenti</p>
                 <div class="user-comments ">
                     <?php if (have_comments()): ?>
@@ -56,4 +32,29 @@
             </div>
         </div>
     </div>
+</div>
+<div class="p-5 text-center my-5 ">
+    <h1>WeTravel i numeri</h1>
+    <div class="d-flex p-5 gap-5 we justify-content-center">
+        <div class="p-5">
+            <h2 class="display-6">250+</h2>
+            <p>Itinerari in tutto il mondo</p>
+
+        </div>
+        <div class="border-start border-end p-5">
+            <h2 class="display-6 ">100.000</h2>
+            <p>Persone in tutta Europa sono già partite con noi </p>
+
+
+        </div>
+        <div class="p-5">
+            <h2 class="display-6">4.7</h2>
+            <p>Gradimento dei nostri viaggi</p>
+
+        </div>
+
+
+
+    </div>
+
 </div>

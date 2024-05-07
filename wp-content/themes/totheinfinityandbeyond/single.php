0451ce37978f73078c9eb84ffc6a78a6fd06ac85
Vorshim92 <vorshim92@gmail.com>
2024-05-07 16:14:00 +0200

    Revert "Merge branch 'alfio2' into vorshim"
    This reverts commit 946fd3509a4fb71ada51b5e2fef8347c3eae3076, reversing
    changes made to 37e033851531b20b263513ef80e06e35cf1985e2.

--- wp-content/themes/totheinfinityandbeyond/single.php
+++ wp-content/themes/totheinfinityandbeyond/single.php
@@ -2,13 +2,13 @@
 
 get_header();
 the_post(); ?>
-<div class="container pt-2 pb-5 ">
+<div class="container">
 
     <h1><?php the_title(); ?></h1>
     <a href="<?php the_author_meta() ?>"></a>
     <img style="width:100% ;max-height:25rem;object-fit:cover" src="<?php the_post_thumbnail_url('custom-cover-thumbnail'); ?>" alt="<?php the_title(); ?>">
     <div class="row pt-4 border-bottom">
-        <div class="col-12 col-md-9">
+        <div class="col-12 col-md-9"> 
 
             <?php the_content(); ?>
         </div>
@@ -17,58 +17,71 @@ the_post(); ?>
 
             <?php
             $current_post_id = get_the_ID();
-            $current_post_categories = wp_get_post_categories($current_post_id);
+            $current_post_categories = wp_get_post_categories($current_post_id)
             ?>
 
-            <div class="row gy-2  ">
+            <div class="row gy-2">
 
 
-                <?php {
-                    $args = array(
-                        'posts_per_page' => 5,
-                        'category__in' => $current_post_categories,
-                        'post__not_in' => array($current_post_id)
-                    );
+                <?php
+             {
+                $args = array(
+                            'posts_per_page' => 5,
+                            'category__in' => $current_post_categories,
+                            'post__not_in' => array($current_post_id)
+                        );
 
-                    $sidebar_posts = new WP_Query($args);
-                    while ($sidebar_posts->have_posts()) :
-                        $sidebar_posts->the_post();
-                ?>
+                $sidebar_posts = new WP_Query($args);
+                while ($sidebar_posts->have_posts()):
+                    $sidebar_posts->the_post();
 
-                        <div class="col-12 border-bottom pb-2 ">
+                  
+
+                        ?>
+                        <div class="col-12 border-bottom pb-2">
                             <div class="row">
                                 <div class="col-6">
-                                    <div class="multiply rounded" style="width: 100%; height: 8rem; background-image: url('<?php the_post_thumbnail_url('thumbnail', ['id' => 'img-state']); ?>');">
+                                    <div class="multiply rounded"
+                                        style="width: 100%; height: 8rem; background-image: url('<?php the_post_thumbnail_url('thumbnail', ['id' => 'img-state']); ?>');">
                                     </div>
                                 </div>
                                 <div class="col-6">
-                                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-                                    <p class="text-secondary">Autore: <?php the_author(); ?></p>
-                                    <p class="text-secondary">Pubblicato il: <?php the_date(); ?></p>
+                                <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                                <p class="text-secondary">Autore: <?php the_author(); ?></p>
+                                <p class="text-secondary">Pubblicato il: <?php the_date(); ?></p>
                                 </div>
                             </div>
+
                         </div>
 
-                <?php
-
-                    endwhile;
-                    wp_reset_postdata();
-                }
-                ?>
+                        <?php
+                    
+                endwhile;
+                wp_reset_postdata(); }
+               ?>
 
             </div>
         </div>
     </div>
 
-
 </div>
 
-<?php if (comments_open() || get_comments_number()) :
-    comments_template();
-endif ?>
+
+<?php
 
 
+?>
 
+<div class="container pt-5">
+    <h5 class="text-secondary">Se hai trovato utile questo articolo</h5>
+
+
+    <?php if (comments_open() || get_comments_number()):
+        comments_template();
+    endif ?>
+
+
+</div>
 
 
 

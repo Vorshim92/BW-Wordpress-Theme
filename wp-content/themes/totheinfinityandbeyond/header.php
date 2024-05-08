4e219b02f9c5df4fef7d622da5594fe058b588f8
Vorshim92 <vorshim92@gmail.com>
2024-05-08 20:22:38 +0200

    fix header home btn

--- wp-content/themes/totheinfinityandbeyond/header.php
+++ wp-content/themes/totheinfinityandbeyond/header.php
@@ -24,11 +24,8 @@
     <nav id="navbar" class="navbar navbar-expand-lg bg-white pt-5">
         <div class="container ">
             <a class="nav-link" href="<?= site_url('/') ?>">
-                <img src="<?php echo get_template_directory_uri(); ?>/assets/logo.png" class="navbar-brand"
-                    alt="<?php bloginfo('name'); ?>"></a>
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
+                <img src="<?php echo get_template_directory_uri(); ?>/assets/logo.png" class="navbar-brand" alt="<?php bloginfo('name'); ?>"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -36,7 +33,7 @@
 
                     <li class="nav-item d-flex align-items-center px-3 border-start">
                         <i class="bi bi-house-fill fs-6"></i>
-                        <a class="nav-link fs-6 fw-semibold text-white" href="<?= site_url('/home') ?>">Home</a>
+                        <a class="nav-link fs-6 fw-semibold text-white" href="<?= site_url('/') ?>">Home</a>
                     </li>
                     <li class="nav-item d-flex align-items-center px-3 border-start">
                         <i class="bi bi-airplane-fill fs-6"></i>
@@ -47,9 +44,7 @@
                         <a class="nav-link fs-6 fw-semibold text-white" href="#ff">Contatti</a>
                     </li>
                     <li class="nav-item d-flex align-items-center px-3 border-start">
-                        <img width="25rem" height="25rem" style="object-fit-cover"
-                            src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/2000px-Flag_of_Italy.svg.png"
-                            alt="" class="rounded-circle me-2">
+                        <img width="25rem" height="25rem" style="object-fit-cover" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/03/Flag_of_Italy.svg/2000px-Flag_of_Italy.svg.png" alt="" class="rounded-circle me-2">
                         <h1 class="m-0 fs-6 fw-bold ">EUR</h1>
                     </li>
 
--- wp-content/themes/totheinfinityandbeyond/single-ttiab_promotions.php
+++ wp-content/themes/totheinfinityandbeyond/single-ttiab_promotions.php
@@ -1,27 +1,52 @@
 <?php get_header(); ?>
 <?php $current_post_id = get_the_ID();
-$current_post_categories = wp_get_post_categories($current_post_id);
+$current_post_categories = wp_get_object_terms($current_post_id, 'location', array('fields' => 'ids'));
 
+// $current_post_categories = wp_get_post_categories($current_post_id, array("cats" => wp_get_object_terms($current_post_id, 'location')));
+
+// $args = array(
+//     'location__in' => $current_post_categories,
+//     'posts_per_page' => 5
+// );
+//
 $args = array(
-    'category__in' => $current_post_categories,
-    'posts_per_page' => 5
+    'posts_per_page' => 5,
+    // 'post__not_in' => array($current_post_id),
+    'tax_query' => array(
+        array(
+            'taxonomy' => 'location',
+            'field' => "term_id",
+            'terms' => $current_post_categories[0], // aggiungere controllo di errore su questo array nel caso in cui mancasse la categoria, quando è senza Locations
+        )
+    )
 );
+
 ?>
 
 <div style="z-index: 1; position: relative;">
     <div class="cover-main" style="background-image: url('<?php the_post_thumbnail_url('custom-cover-thumbnail', ['id' => 'img-state']); ?>');">
-        <div class="cover-contents">
-            <h1 class="trip-title">CIAONE</h1>
-            <h3 class="trip-overview">ANCHE A TE</h3>
+        <div class="cover-contents w-100">
+            <div class="text-center">
+                <h1 class="display-2">Il Desiderio di Viaggiare Inizia Qui</h1>
+                <h1 class="trip-title bebas">
+                    <?php the_title(); ?>
+                </h1>
+                <h2 class="display-6 mt-5">Ogni Destinazione, una Storia da Raccontare </h2>
+                <p class="">Viaggi pensati e progettati dai nostri Coordinatori </p>
+                <div class="d-flex gap-5 justify-content-center">
+                    <i class="bi bi-airplane fs-12"></i>
+                    <i class="bi bi-people fs-12"></i>
+                    <i class="bi bi-emoji-laughing fs-12"></i>
+                    <i class="bi bi-flag fs-12"></i>
+                    <!-- <img src="/assets/word.svg.svg" alt="" srcset=""> -->
+                </div>
+
+
+            </div>
+            <!-- <h3 class="trip-overview">ANCHE A TE</h3> -->
 
             <div style="padding-top: 8px;">
-                <button class="btn-play">
-                    <i class="fa fa-play"></i> Play
-                </button>
 
-                <button class="btn-more">
-                    <i class="fa fa-info-circle"></i> More Info
-                </button>
             </div>
         </div>
     </div>
@@ -35,17 +60,21 @@ $args = array(
         </div>
 
         <div class="col-12 col-md-3">
-            <p class="text-secondary">Altri Hotel per te</p>
-
+            <p class="text-secondary">Hotel suggeriti per te</p>
+            <img src="https://images.unsplash.com/photo-1496417263034-38ec4f0b665a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWx8ZW58MHx8MHx8fDA%3D" class="img-fluid mb-2" alt="...">
+            <img src="https://images.unsplash.com/photo-1566073771259-6a8506099945?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aG90ZWx8ZW58MHx8MHx8fDA%3Dc" class="img-fluid mb-2" alt="...">
+            <img src="https://plus.unsplash.com/premium_photo-1678297269980-16f4be3a15a6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWx8ZW58MHx8MHx8fDA%3D" class="img-fluid mb-2" alt="...">
+            <p class="text-secondary mt-5">Ristoranti suggeriti per te</p>
+            <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmlzdG9yYW50aXxlbnwwfHwwfHx8MA%3D%3D" class="img-fluid mb-2" alt="...">
+            <img src="https://images.unsplash.com/photo-1552566626-52f8b828add9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmlzdG9yYW50aXxlbnwwfHwwfHx8MA%3D%3D" class="img-fluid mb-2" alt="...">
             <?php
 
 
-            $sidebar_posts = new WP_Query(array(
-                'category__in' => $current_post_categories,
-                'posts_per_page' => 5,
-                'post_type' => 'ttiab_promotions',
-                'post__not_in' => array($current_post_id)
-            ));
+            $sidebar_posts = new WP_Query(
+                $args + array(
+                    'post_type' => 'ttiab_promotions'
+                )
+            );
             while ($sidebar_posts->have_posts()) :
                 $sidebar_posts->the_post();
 
@@ -74,12 +103,7 @@ $args = array(
 
             <?php
 
-
-            $sidebar_posts = new WP_Query(array(
-                'category__in' => $current_post_categories,
-                'posts_per_page' => 5,
-                'post_type' => 'ttiab_reviews'
-            ));
+            $sidebar_posts = new WP_Query($args + array('post_type' => 'ttiab_reviews'));
             while ($sidebar_posts->have_posts()) :
                 $sidebar_posts->the_post();
 
@@ -113,16 +137,15 @@ $args = array(
 
     <!-- Inizio articoli -->
     <div class="row py-3 gy-4">
+        <div class="col-12 col-md-8">
+            <div class="row gy-3">
+                <p class="text-secondary">Leggi gli articoli correlati a questo viaggio</p>
+                <?php
 
-        <?php
 
+                $query = new WP_Query($args + array('post_type' => 'post'));
+                if ($query->have_posts()) { ?>
 
-
-        $query = new WP_Query($args);
-        if ($query->have_posts()) { ?>
-            <div class="col-12 col-md-8">
-                <div class="row gy-3">
-                    <p class="text-secondary">Leggi gli articoli correlati a questo viaggio</p>
                     <?php while ($query->have_posts()) {
                         $query->the_post(); ?>
                         <div class="col-6 col-lg-4 col-xl-3">
@@ -137,50 +160,19 @@ $args = array(
                             </div>
                         </div>
                     <?php } ?>
-                </div>
+
+                <?php }
+                wp_reset_postdata();
+
+                ?>
             </div>
-        <?php }
-        wp_reset_postdata();
-
-        ?>
-
-        <div class="col-12 col-md-4 border p-4 rounded">
-            <?php
-            /*
-            Template Name: Aggiungi Articolo
-            */
-            if (isset($_POST['submit'])) {
-                $titolo = sanitize_text_field($_POST['titolo']);
-                $contenuto = wp_kses_post($_POST['contenuto']);
-                $nuovo_articolo = array(
-                    'post_title' => $titolo,
-                    'post_content' => $contenuto,
-                    'post_status' => 'pending',
-                    'post_author' => get_current_user_id(),
-                    'post_type' => 'ttiab_reviews'
-                );
-                $post_id = wp_insert_post($nuovo_articolo);
-                if ($post_id) {
-                    echo "Articolo pubblicato con successo!";
-                } else {
-                    echo "Si è verificato un errore durante la pubblicazione dell'articolo.";
-                }
-            }
-            ?>
-            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-                <p class="text-secondary">Hai vissuto questa esperienza con noi e vuoi pubblicare il tuo articolo?</p>
-                <p class="text-secondary">qui puoi farlo <a href="#" class="fw-light">Leggi la politica delle recensioni</a> </p>
-                <form method="post" action="">
-                    <label for="titolo">Titolo:</label><br>
-                    <input type="text" id="titolo" name="titolo" required><br><br>
-
-                    <label for="contenuto">Contenuto:</label><br>
-                    <textarea id="contenuto" name="contenuto" style="width:100%;height:10rem" required></textarea><br><br>
-
-                    <input type="submit" name="submit" value="Pubblica Articolo" style="background-color:#E09F05" class="border-0 text-white rounded p-1">
-                </form>
-            </article><!-- #post-<?php the_ID(); ?> -->
         </div>
+
+    </div>
+    <div class="col-12">
+        <?php
+        echo do_shortcode('[custom_add_review]');
+        ?>
     </div>
 </div>
 

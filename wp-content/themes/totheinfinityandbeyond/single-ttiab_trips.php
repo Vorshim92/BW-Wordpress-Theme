8a50a01aa550c522536f3653fa5eab277664d8bd
Vorshim92 <vorshim92@gmail.com>
2024-05-08 19:53:46 +0200

    todo

--- wp-content/themes/totheinfinityandbeyond/single-ttiab_trips.php
+++ wp-content/themes/totheinfinityandbeyond/single-ttiab_trips.php
@@ -16,7 +16,7 @@ $args = array(
         array(
             'taxonomy' => 'location',
             'field' => "term_id",
-            'terms' => $current_post_categories[0],
+            'terms' => $current_post_categories[0], // aggiungere controllo di errore su questo array nel caso in cui mancasse la categoria, quando è senza Locations
         )
     )
 );
@@ -24,8 +24,7 @@ $args = array(
 ?>
 
 <div style="z-index: 1; position: relative;">
-    <div class="cover-main"
-        style="background-image: url('<?php the_post_thumbnail_url('custom-cover-thumbnail', ['id' => 'img-state']); ?>');">
+    <div class="cover-main" style="background-image: url('<?php the_post_thumbnail_url('custom-cover-thumbnail', ['id' => 'img-state']); ?>');">
         <div class="cover-contents w-100">
             <div class="text-center">
                 <h1 class="display-2">Il Desiderio di Viaggiare Inizia Qui</h1>
@@ -62,32 +61,29 @@ $args = array(
 
         <div class="col-12 col-md-3">
             <p class="text-secondary">Hotel suggeriti per te</p>
-            <img src="https://images.unsplash.com/photo-1496417263034-38ec4f0b665a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWx8ZW58MHx8MHx8fDA%3D"
-                class="img-fluid mb-2" alt="...">
-            <img src="https://images.unsplash.com/photo-1566073771259-6a8506099945?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aG90ZWx8ZW58MHx8MHx8fDA%3Dc"
-                class="img-fluid mb-2" alt="...">
-            <img src="https://plus.unsplash.com/premium_photo-1678297269980-16f4be3a15a6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWx8ZW58MHx8MHx8fDA%3D"
-                class="img-fluid mb-2" alt="...">
+            <img src="https://images.unsplash.com/photo-1496417263034-38ec4f0b665a?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Nnx8aG90ZWx8ZW58MHx8MHx8fDA%3D" class="img-fluid mb-2" alt="...">
+            <img src="https://images.unsplash.com/photo-1566073771259-6a8506099945?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8aG90ZWx8ZW58MHx8MHx8fDA%3Dc" class="img-fluid mb-2" alt="...">
+            <img src="https://plus.unsplash.com/premium_photo-1678297269980-16f4be3a15a6?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aG90ZWx8ZW58MHx8MHx8fDA%3D" class="img-fluid mb-2" alt="...">
             <p class="text-secondary mt-5">Ristoranti suggeriti per te</p>
             <img src="https://images.unsplash.com/photo-1517248135467-4c7edcad34c4?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8cmlzdG9yYW50aXxlbnwwfHwwfHx8MA%3D%3D" class="img-fluid mb-2" alt="...">
             <img src="https://images.unsplash.com/photo-1552566626-52f8b828add9?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8cmlzdG9yYW50aXxlbnwwfHwwfHx8MA%3D%3D" class="img-fluid mb-2" alt="...">
             <?php
 
 
-            $sidebar_posts = new WP_Query($args + array(
-                'post_type' => 'ttiab_promotions'
-            )
+            $sidebar_posts = new WP_Query(
+                $args + array(
+                    'post_type' => 'ttiab_promotions'
+                )
             );
-            while ($sidebar_posts->have_posts()):
+            while ($sidebar_posts->have_posts()) :
                 $sidebar_posts->the_post();
 
 
-                ?>
+            ?>
                 <div class="col-12 border-bottom pb-2">
                     <div class="row ">
                         <div class="col-6">
-                            <div class="multiply rounded"
-                                style="width: 100%; height: 8rem; background-image: url('<?php the_post_thumbnail_url('custom-cover-thumbnail', ['id' => 'img-state']); ?>');">
+                            <div class="multiply rounded" style="width: 100%; height: 8rem; background-image: url('<?php the_post_thumbnail_url('custom-cover-thumbnail', ['id' => 'img-state']); ?>');">
                             </div>
                         </div>
                         <div class="col-6">
@@ -98,7 +94,7 @@ $args = array(
 
                 </div>
 
-                <?php
+            <?php
 
             endwhile;
             wp_reset_postdata();
@@ -108,16 +104,15 @@ $args = array(
             <?php
 
             $sidebar_posts = new WP_Query($args + array('post_type' => 'ttiab_reviews'));
-            while ($sidebar_posts->have_posts()):
+            while ($sidebar_posts->have_posts()) :
                 $sidebar_posts->the_post();
 
 
-                ?>
+            ?>
                 <div class="col-12 border-bottom pb-2">
                     <div class="row ">
                         <div class="col-6">
-                            <div class="multiply rounded"
-                                style="width: 100%; height: 8rem; background-image: url('<?php the_post_thumbnail_url('custom-cover-thumbnail', ['id' => 'img-state']); ?>');">
+                            <div class="multiply rounded" style="width: 100%; height: 8rem; background-image: url('<?php the_post_thumbnail_url('custom-cover-thumbnail', ['id' => 'img-state']); ?>');">
                             </div>
                         </div>
                         <div class="col-6">
@@ -128,7 +123,7 @@ $args = array(
 
                 </div>
 
-                <?php
+            <?php
 
             endwhile;
             wp_reset_postdata();
